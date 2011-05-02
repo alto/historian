4d77d3fe06ee0dd7c824c76afb7f22d169dc5aa1
Thorsten Böttger <alto@mt7.de>
2011-05-02 23:10:20 +0200

    deliver static asset files from the gem's public directory as well

--- lib/historian/engine.rb
+++ lib/historian/engine.rb
@@ -2,5 +2,9 @@ require 'rails'
 
 module Historian
   class Engine < Rails::Engine
+    initializer "static assets" do |app|
+      # include the gem's public directory for asset files
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+    end
   end
 end
